1b8c204610c758dcee3737b43392302458f383df
sazibrahman38-beep <sazibrahman38@gmail.com>
2026-08-23 15:11:01 +0600

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,25 @@ function prevPage() {
             pages.length - currentPage;
     }
 }
+let startX = 0;
+
+document.querySelector(".book").addEventListener("touchstart", function(e) {
+    startX = e.touches[0].clientX;
+});
+
+document.querySelector(".book").addEventListener("touchend", function(e) {
+
+    let endX = e.changedTouches[0].clientX;
+    let difference = startX - endX;
+
+    // Swipe left → Next page
+    if (difference > 50) {
+        nextPage();
+    }
+
+    // Swipe right → Previous page
+    if (difference < -50) {
+        prevPage();
+    }
+
+});
